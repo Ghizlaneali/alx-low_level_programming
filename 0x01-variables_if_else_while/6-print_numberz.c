4f94a63af21c43d4f13a84e09ae5dbba8d83536d
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-06 11:57:07 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print single digits of base 10 setarting from 0
- * 	using only putchar, followed by a new line.
+ *	using only putchar, followed by a new line.
  *
  * Return:Always 0.
  */
@@ -10,10 +10,10 @@
 int main(void)
 {
 	int num;
-	for (num = 0; num < 10; num++)
+	for(num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	putchar('\n');
 
 	return (0);
-}	
+}
