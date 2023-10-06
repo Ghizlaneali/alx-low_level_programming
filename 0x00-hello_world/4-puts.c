e9c669c2370a6eea8240df499f2334ab95376ea7
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-06 05:38:01 -0700

    first day of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Main block
+ *
+ * Return : 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
