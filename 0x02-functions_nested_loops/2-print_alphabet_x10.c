b0387a8100905ecde6ac03456d8de75c0501aba0
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-10 16:04:39 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,25 +3,24 @@
 /**
  * print_alphabet_x10 - Print the alphabet 10 times in lower case
  *	, followed by a new line.
- *
- * Return : void.
  */
 
 void print_alphabet_x10(void)
 {
-	char c;
+	char ch;
 	int i;
 
 	i = 0;
 
-	while (i > 10)
+	while (i < 10)
 	{
-		c = 'a';
-		for (c = 'a'; c <= 'z'; c++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(c);
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
