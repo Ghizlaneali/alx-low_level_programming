9d5311948d5010b24185ce0c2396bf59e125627d
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-10 12:33:08 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
 *print_alphabet - Print the alphabet in lowercase, followed by a new line.
 *
 *Return: void.
 */
-int _putchar(char letter)
-{
-	return (write(1, &letter, 1));
-}
 
 void print_alphabet(void)
 {
