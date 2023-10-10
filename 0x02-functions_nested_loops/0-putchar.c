e59c4f6d8c7105cc458d82ca876c543e9720f5f6
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-10 07:49:09 -0700

    nested loops and functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Print "_putchar", followed by a new line.
+*
+*Return: Alaways 0.
+*/
+
+int main(void)
+{
+	putchar ("_");
+	putchar ("p");
+	putchar ("u");
+	putchar ("t");
+	putchar ("c");
+	putchar ("h");
+	putchar ("a");
+	putchar ("r");
+	putchar ("\n");
+
+	return(0);
+}
