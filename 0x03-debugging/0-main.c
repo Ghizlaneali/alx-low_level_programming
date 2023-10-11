4ef6febd136b534ccda1597576ef583cde163879
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-11 09:06:57 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks if the int is positive or negative.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
