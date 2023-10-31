443d44787934caf3569336aae80f789e564ce5dc
Ghizlaneali <ghizlaneali76@gmail.com>
2023-10-31 13:14:25 -0700

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,13 +16,12 @@ char *str_concat(char *s1, char *s2)
 	size_t i = 0;
 	char *d;
 
+	d = (char *)malloc(sizeof(s1) + sizeof(s2) + 1);
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-
-	d = (char *)malloc(sizeof(s1) + sizeof(s2) + 1);
 
 	while (s1 != NULL)
 	{
