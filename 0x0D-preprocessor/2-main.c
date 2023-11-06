1e3d3dac0714bc794da9d29ad33235ce615f5dd8
Ghizlaneali <ghizlaneali76@gmail.com>
2023-11-06 03:11:44 -0800

    preprocessors

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,12 @@
 
 /**
  * main - entry point
+ *
+ * Return : Always 0.
  */
 
 void main()
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this is a read me file for the 0x0D. C - Preprocessor tasks
